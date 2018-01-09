1514cb3b5c21de932305e3859a9c168101688e2e
OganM <ogan.mancarcii@gmail.com>
2018-01-08 22:20:32 -0800

    minor progress

--- test.R
+++ test.R
@@ -13,7 +13,40 @@ Sys.getenv(x = 'JAVA_HOME', unset = "", names = NA)
 
 
 
-ermineR = function(annotation, # can be a file or a DF
+
+#' Run ermineJ analysis
+#'
+#' @param annotation Annotation. A file path, a data.frame or a platform short 
+#' name (eg. GPL127). If given a platform short name it will be downloaded
+#' from annotation repository of Pavlidis Lab (ErmineJ format). Note that if there
+#' is a file with the same name as the 
+#' @param annotationFormat Format of the annotation. \code{"ErmineJ"} for the 
+#' format of the files provided by Pavlidis Lab. \code{"Agilent"} and 
+#' \code{"Affymetrix"} for files provided by the 
+#' @param bigIsBetter 
+#' @param configFile 
+#' @param geneSetDescription 
+#' @param scoreFile 
+#' @param scoreColumn 
+#' @param filterNonSpecific 
+#' @param geneReplicates 
+#' @param iterations 
+#' @param genesOut 
+#' @param logTrans 
+#' @param pAdjust 
+#' @param test 
+#' @param multifunctionalityCorrection 
+#' @param outFile 
+#' @param quantile 
+#' @param expression 
+#' @param minClassSize 
+#' @param maxClassSize 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ermineR = function(annotation, # can be a file path, data.frame or the platform short name
                    annotationFormat = c('ErmineJ','Affy CSV','Agilent'), # not sure how to set up for agilent
                    bigIsBetter = FALSE, 
                    configFile = NULL,
@@ -41,6 +74,15 @@ ermineR = function(annotation, # can be a file or a DF
     
     ermineJHome = system.file("ermineJ-3.0.3",package = 'ermineR')
     Sys.setenv(ERMINEJ_HOME = ermineJHome)
+    
+    if(class(annotation)=='character'){
+        if(!file.exists(annotation)){
+            plaftorm = tryCatch(XML::getHTMLLinks('http://www.chibi.ubc.ca/microannots'),
+                                          error = function(e){
+                                              stop('"annotation" is not a valid file or exists in Pavlidis lab annotations. See http://www.chibi.ubc.ca/microannots for available options.')
+                                          })
+        }
+    }
     
     
     
