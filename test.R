13a38a73c310814d72619f3a998656df663d7c58
OganM <ogan.mancarcii@gmail.com>
2018-01-08 21:57:34 -0800

    build function parameters. will be re-ordeded

--- test.R
+++ test.R
@@ -13,11 +13,36 @@ Sys.getenv(x = 'JAVA_HOME', unset = "", names = NA)
 
 
 
-ermineR = function(annotationFile,
-                   annotationFormat = c('ErmineJ','Affy CSV','Agilent'),
-                   bigIsBetter = FALSE,
+ermineR = function(annotation, # can be a file or a DF
+                   annotationFormat = c('ErmineJ','Affy CSV','Agilent'), # not sure how to set up for agilent
+                   bigIsBetter = FALSE, 
                    configFile = NULL,
-                   classFile,
+                   geneSetDescription = 'Latest_GO', # if "Latest_GO" download the latest one, otherwise provide file path
                    # dataDir = NULL,
-                   scoreColumn,
-                   )
+                   scoreFile = NULL,
+                   scoreColumn = 2,
+                   filterNonSpecific = TRUE,
+                   geneReplicates = c('mean','best'),
+                   iterations = NULL,
+                   genesOut = FALSE,
+                   logTrans = FALSE,
+                   pAdjust = c('FDR','Westfall-Young'),
+                   test = c('ORA','GSR','CORR','ROC'),
+                   multifunctionalityCorrection = TRUE,
+                   outFile,
+                   quantile = 50,
+                   expression,
+                   minClassSize = 10,
+                   maxClassSize =100){
+    
+    # iterations are mandatory for GSR and CORR
+    # scoreFile and scoreColumn mandatory for  all but CORR
+    # expression is only used by CORR
+    
+    ermineJHome = system.file("ermineJ-3.0.3",package = 'ermineR')
+    Sys.setenv(ERMINEJ_HOME = ermineJHome)
+    
+    
+    
+    
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ermineR)
+
+test_check("ermineR")
