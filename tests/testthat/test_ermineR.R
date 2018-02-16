24d6392157a65355dd0af3009f03a2322d4e50e8
OganM <ogan.mancarcii@gmail.com>
2018-02-16 14:14:27 -0800

    test with local go

--- tests/testthat/test_ermineR.R
+++ tests/testthat/test_ermineR.R
@@ -1,19 +1,23 @@
 context('ermineJ usage')
 
+# utils::download.file('http://archive.geneontology.org/latest-termdb/go_daily-termdb.rdf-xml.gz',destfile = 'GO.xml.gz',quiet= TRUE)
+
 test_that('ermineJ basic usage',{
     annotation = 'Generic_human.txt'
     scores <-read.table("scoreFile.txt", header=T, row.names = 1)
     
     result1 = ermineR(annotation = 'Generic_human.txt',
+                      geneSetDescription = 'GO.xml.gz',
                       scoreColumn = 2,
                       scores = scores,
                       output = 'out',
-                      return = TRUE)
+                      return = TRUE,)
     testthat::expect_is(result1,'list')
     testthat::expect_is(result1$results,'data.frame')
     
     # check column names and integers give the same results
     result2 = ermineR(annotation = 'Generic_human.txt',
+                      geneSetDescription = 'GO.xml.gz',
                       scoreColumn = 'Endothelial',
                       scores = scores,
                       output = 'out',
@@ -26,6 +30,7 @@ test_that('ermineJ basic usage',{
     # paul's test
     scores<-read.table("chd8.pvals.txt", header=T, row.names = 1)
     lmebtr.enrich<-ermineR(annotation = "Generic_mouse", 
+                           geneSetDescription = 'GO.xml.gz',
                            scores=scores,
                            scoreColumn = 6, 
                            test="GSR", 
